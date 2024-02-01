f22b2e06e64fc9bdfcdbfb004593f1f88f912ca4
Sergey Chepelev <kabawonga@gmail.com>
2024-02-01 19:18:53 +0600

    1-10 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Technologies } from './Technologies'
+import { Header } from './Header'
+import { Footer } from './Footer'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      <Header />
+      <Technologies />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Technologies.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Technologies: React.FC = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  )
+}
